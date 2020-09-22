dba0135ffd3bbfb5c0ce3c3443949ef1f6f0f4d9
Andrei Biro <andreibiro2003@gmail.com>
2020-09-22 13:20:10 +0300

    incepe iara sa mearga

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,19 +1,36 @@
 #include <Servo.h>
 
+int input;
 Servo servo;
-
+int led = 2;
 void setup() {
   // put your setup code here, to run once:
-  servo.attach(9);
+  //Sservo.attach(9);
   Serial.begin(9600);
-  
+  servo.attach(9);
+  // calibrare
+  Serial.println("calibrating...");
+  for(int i=0; i<180;i++){
+      servo.write(i);
+    }
+  for(i=180; i>0;i--){
+      servo.write(i);
+    }
+  Serial.println("Done calibrating...");
+  servo.write(90);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if(Serial.available()>0){
-      int input = int(Serial.read());
-      servo.write(input);                                
+     if(servo.attached()!= true) servo.attach(9);
+     
+      input = Serial.parseInt();
+      servo.write(input);
+      delay(10);
+      Serial.println(input);
     }
-  
+  else{
+      servo.detach();
+    }
 }
--- tracking.py
+++ tracking.py
@@ -1,19 +1,67 @@
-import cv2
+from cv2 import cv2
+import serial
+import time
+
+s = serial.Serial('COM4',9600,timeout=.1)
 print("Starting")
 
+panAngle=90
+s.write(90)
 cap = cv2.VideoCapture(0)
-#tracker = cv2.TrackerMOSSE_create()
-#tracker = cv2.TrackerMedianFlow_create()
-tracker =  cv2.TrackerBoosting_create()
 succes, img = cap.read()
+tracker = cv2.TrackerMOSSE_create()
+#tracker = cv2.TrackerCRST_create()
 bbox=cv2.selectROI("Tracking",img,False)
 tracker.init(img,bbox)
 
+width_camera =int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+window_height,window_width,channels = img.shape
 
 
 def drawBox(img,bbox):
+
     x,y,w,h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3]),
     cv2.rectangle(img,(x,y),((x+w),(y+h)),(x,y,x*y/255),3,1)
+    #print(cv2.width)
+    mapServoPosition(x)
+
+
+def mapServoPosition (x):
+    global panAngle
+    if (x < int((window_width/2)-75)):
+        panAngle-=1
+        if panAngle < 10 :
+            panAngle=10
+        positionServo(panAngle)
+        print(panAngle)
+    if (x > int(window_width/2)+75):
+        panAngle+=1
+        if panAngle > 170:
+            panAngle = 170
+        positionServo(panAngle)
+        print(panAngle)
+    time.sleep(1)
+    #panAngle=x
+    
+    #if (x < (width_camera/2)-75):
+     #   panAngle += 10
+      #  if panAngle > 170:
+       #     panAngle = 170
+       # positionServo (panAngle)
+       # print(panAngle)
+    #if (x > (width_camera/2)+75):
+     #   panAngle -= 10
+      #  if panAngle < 10:
+       #     panAngle = 10
+        #positionServo (panAngle)
+        #print(panAngle)
+
+    #if x <10:
+     #   panAngle=10
+    
+def positionServo(pan):
+    s.write(int(pan))
+
 
 while True:
     timer = cv2.getTickCount()
@@ -24,15 +72,19 @@ while True:
     else:
         cv2.putText(img,"Lost",(75,75),cv2.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),2)
         
-
-
     #cv2.imshow("Tracking",img)
     fps=cv2.getTickFrequency()/(cv2.getTickCount()-timer)
     cv2.putText(img,str(int(fps)),(75,50),cv2.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),2)
+    cv2.line(img,(int(window_width/2-75),0),(int(window_width/2-75),int(window_height)),(255,0,75),2)
+    cv2.line(img,(int(window_width/2+75),0),(int(window_width/2+75),int(window_height)),(255,0,75),2)
     cv2.imshow("Tracking",img)
-    if cv2.waitKey(1) & 0xff ==ord('a'):
+    if cv2.waitKey(1) & 0xff== ord('r'):
+        s.write(int(90))
+        pass
+    if cv2.waitKey(1) & 0xff == ord('a'):
         break
-    if cv2.waitKey(1) & 0xff==ord('t'):
+    if cv2.waitKey(1) & 0xff== ord('t'):
         tracker = cv2.TrackerMOSSE_create()
         bbox = cv2.selectROI("Tracking", img, False)
         tracker.init(img, bbox)
+   
