ed0dbaed409efc6750c28bbf8cc782cabb8b1405
Andrei Biro <andreibiro2003@gmail.com>
2020-09-22 10:20:53 +0300

    arduino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,13 +1,19 @@
 #include <Servo.h>
 
+Servo servo;
 
 void setup() {
   // put your setup code here, to run once:
+  servo.attach(9);
   Serial.begin(9600);
-  Servo servo 
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if(Serial.available()>0){
+      int input = int(Serial.read());
+      servo.write(input);                                
+    }
   
 }
