9ef8b66192529a9d6f79209578f017a7c11e05ef
Andrei Biro <andreibiro2003@gmail.com>
2020-09-22 10:10:15 +0300

    das

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,10 +1,13 @@
+#include <Servo.h>
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  Servo servo 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
